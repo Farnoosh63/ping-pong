e8b400c3e25a3d751a12737e1988c6ef2949b62f
farnoosh <farnoosh@pdx.edu>
2016-02-24 20:15:12 -0800

    seperate the pingPongFunction from jQuery part in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+var pingPongFunction = function (number) {
+  var listResult="";
+
+    for(var i=1;i<= number; i++){
+      if((i%3===0)&&(i%5===0)){
+        listResult += "pingpong";
+      }else if (i%3===0) {
+        listResult += "ping";
+      }else if (i%5===0) {
+        listResult += "pong";
+      }else {
+        listResult += i;
+      }
+      listResult += "<br>"
+    } //end of for loop
+    return listResult;
+  }
+
 $(document).ready(function(){
   $("form#search").submit(function(event){
     var userType = $("input#something").val();
@@ -6,32 +24,20 @@ $(document).ready(function(){
   })
   $(".container").submit(function pingpong(event){
     var userInput = parseInt($("input#form-control").val());
-    var listResult = "";
-
 
     if (userInput <= 0){
       $("#results").hide();
       $(".error").show();
     }else if (userInput > 0){
-      for(var i=1;i<= userInput; i++){
-        if((i%3===0)&&(i%5===0)){
-          listResult += "pinGPong"+'<br>';
-        }else if (i%3===0) {
-          listResult += "ping"+'<br>';
-        }else if (i%5===0) {
-          listResult += "pong"+'<br>';
-        }else {
-          listResult += i+"<br>";
-        }
-      } //end of for loop
-      document.getElementById("results").innerHTML = listResult;
+      var result = pingPongFunction(userInput);
+      // var collection =
+      document.getElementById("results").innerHTML = result;
       $(".error").hide();
       $("#results").show();
     } else {
       alert("There is a problem with your input. A force refresh will occur, please try again!");
       location.reload();
     }
-
     event.preventDefault();
   }); //end of form-inline submit
 });  //end of document ready
